ca131b5b1f4338a9c1f50fd3f5c4b47da4d00ad5
casino-tango <unicasinosdecombiasa@gmail.com>
2024-03-07 10:56:42 -0500

    prueba de vercel5

--- src/database/database.js
+++ src/database/database.js
@@ -19,30 +19,6 @@ import { POSTGRES_URL} from "../config/config.js";
 
 //  *******conexion con vercel****
 
-// export const sequelize = new Sequelize({
-//   dialect: 'postgres',
-//   host: process.env.DATABASE_HOST,
-//   port: process.env.DB_PORT, // Utiliza la variable de entorno para el puerto
-//   database: process.env.DATABASE_DATABASE,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
-
-// export const sequelize = new Sequelize(
-//   process.env.DATABASE_DATABASE,
-//   process.env.DATABASE_USERNAME,
-//   process.env.DATABASE_PASSWORD,                    
-//    {
-//     host: process.env.DATABASE_HOST,
-//   dialect: 'postgres',
-// });
-
 export const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.DATABASE_HOST,
@@ -56,7 +32,16 @@ export const sequelize = new Sequelize({
       rejectUnauthorized: false
     }
   }
-})
+});
+
+// export const sequelize = new Sequelize(
+//   process.env.DATABASE_DATABASE,
+//   process.env.DATABASE_USERNAME,
+//   process.env.DATABASE_PASSWORD,                    
+//    {
+//     host: process.env.DATABASE_HOST,
+//   dialect: 'postgres',
+// });
 
 
 async function testConnection() {
